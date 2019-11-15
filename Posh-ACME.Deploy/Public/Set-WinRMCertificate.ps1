4ab7e35a9df0baa3eb5113d99a279e01044c1229
Ryan Bolger <z97y9drumh@snkmail.com>
2019-11-15 15:24:00 -0800

    improved comments

--- Posh-ACME.Deploy/Public/Set-WinRMCertificate.ps1
+++ Posh-ACME.Deploy/Public/Set-WinRMCertificate.ps1
@@ -38,14 +38,17 @@ function Set-WinRMCertificate {
             # save the old thumbprint
             $oldThumb = $listener.CertificateThumbprint
 
-            # get the hostname from the cert subject
+            # Set-WSManInstance fails if the existing hostname on the listener doesn't match
+            # the CN value of the certificate's subject (which seems broken to me, but *shrug*).
+            # So we're going to parse the hostname from the subject and make sure we set it
+            # at the same time as the thumbprint.
             $subject = (Get-Item "Cert:\LocalMachine\My\$CertThumbprint").Subject
             if ($subject -match 'CN=(?<host>[^,]+)') {
                 $certHost = $matches['host']
-                Write-Verbose "Parsed hostname $certHost"
+                Write-Verbose "Parsed hostname $certHost from cert subject."
             }
 
-            # set the new one
+            # set the new thumbprint
             Write-Verbose "Setting Listener with Transport $Transport and Address $Address to certificate thumbprint $CertThumbprint and hostname $certHost."
             $setParams = @{
                 ResourceURI = 'winrm/config/Listener'
