64953129e5aa31fd6db8100b027a30c103cc489e
Mark Pizzolato <mark@infocomm.com>
2021-12-23 13:07:43 -0800

    Manage Install and Removal of Exchange certificate consistently
    - Change the default Exchange Services to the normal full set of
      services Exchange uses certificated for: IMAP, POP, IIS and SMTP
    - Install the new certificated with the specified services enabled.
    - If the Exchange certificated for the specified services is successfully
      installed, then the prior certificate is removed from the Exchange
      configuration.  The prior certificate is left in the local machine's
      certificate store unless -RemoveOldCert is specified.
    - If an Exchange certificate is installed for a service set which
      includes IIS, restart IIS so that it will be using the newly installed
      cert.

--- Posh-ACME.Deploy/Public/Set-ExchangeCertificate.ps1
+++ Posh-ACME.Deploy/Public/Set-ExchangeCertificate.ps1
@@ -8,26 +8,25 @@ function Set-ExchangeCertificate {
         [string]$PfxFile,
         [Parameter(Position=2,ValueFromPipelineByPropertyName)]
         [securestring]$PfxPass,
-        [string[]]$ExchangeServices=@('IIS','SMTP'),
+        [string[]]$ExchangeServices=@('IMAP', 'POP', 'IIS','SMTP'),
         [switch]$RemoveOldCert
     )
 
     Begin {
+    
         # make sure the Exchange snapin is available on the local system
         if (!(Get-PSSnapin -Registered | Where-Object { $_.Name -match "Microsoft.Exchange.Management.PowerShell" })) {
-            try { throw "The Microsoft.Exchange.Management.PowerShell snapin is required to use this function." }
-            catch { $PSCmdlet.ThrowTerminatingError($_) }
+            throw "The Microsoft.Exchange.Management.PowerShell snapin is required to use this function."
         } else {
-            # if it's not already loaded
             if (!(Get-PSSnapin | Where-Object {
                   $_.Name -match "Microsoft.Exchange.Management.PowerShell" -and (
                       $_.Name -match "Admin" -or
                       $_.Name -match "E2010" -or
                       $_.Name -match "SnapIn"
                   )
-            })) { # load it
-                Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
-            }
+            })) {
+                 Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+                 }
         }
     }
 
@@ -38,23 +37,67 @@ function Set-ExchangeCertificate {
 
         $CertThumbprint = Confirm-CertInstall @PSBoundParameters
 
-        # check the old thumbprint value
-        $oldThumbs = ($ExchangeServices | ForEach-Object {
-            $service = $_
-            (Get-ExchangeCertificate | Where-Object { $service -in ($_.Services -split ', ') }).Thumbprint
-        } | Sort-Object -Unique)
+        $Cert = Get-ChildItem -Path "Cert:\LocalMachine\My\$CertThumbprint"
+        $CertExpire = $Cert.NotBefore
 
-        if ($CertThumbprint -notin $oldThumbs) {
+        # find the old thumbprint value for the specified services
+        $OldCertThumbprint = ''
+        $OldCertSubject = ''
+        $OldCertExpire = ''
+        $ExchangeCerts = Get-ExchangeCertificate 
+        $ExchangeCerts | ForEach-Object {
+            $CertServices = $_.Services -split ', '
+            $ServicesDiff = Compare-Object -ReferenceObject $ExchangeServices -DifferenceObject $CertServices
+            if ($ServicesDiff.Length -eq 0) { # No differences? means same services
+                $OldCertThumbprint = $_.Thumbprint.ToString()
+                $OldCertSubject = $_.Subject.ToString()
+                $OldCertExpire = $_.NotAfter
+            }
+        }
 
-            # set the new value
-            Write-Verbose "Setting new Exchange thumbprint value"
-            Enable-ExchangeCertificate -Services $ExchangeServices -Thumbprint $CertThumbprint -Force -EA Stop -Verbose:$false
+        $NewCertInstalled = $false
 
-            # remove the old certs if specified
-            if ($RemoveOldCert) { $oldThumbs | ForEach-Object { Remove-OldCert $_ } }
+        if ($OldCertThumbprint -eq $CertThumbprint) { # Already registered in Exchange?
+
+            Write-Host "Specified certificate ($OldCertSubject, Expiring: $OldCertExpire) is already configured for Exchange services: $ExchangeServices."
 
         } else {
-            Write-Warning "Specified certificate is already configured for Exchange"
+
+            try {
+
+                # set the new value
+                Write-Host "Setting new Exchange thumbprint: $CertThumbprint value Expiring: $CertExpire"
+                Enable-ExchangeCertificate -Services $ExchangeServices -Thumbprint $CertThumbprint -Force -EA Stop -Verbose:$false
+
+                $NewCertInstalled = $true
+
+            } catch { throw }
+
+        }
+
+        # automatically unconfigure old certs for the current service list from Exchange
+        $ExchangeCerts | ForEach-Object {
+            $ThisServices = $_.Services -split ', '
+            $ThisSubject = $_.Subject
+            $ThisNotAfter = $_.NotAfter
+            $ThisThumbprint = $_.Thumbprint
+            $ServicesDiff = Compare-Object -ReferenceObject $ExchangeServices -DifferenceObject $ThisServices
+            if (($ServicesDiff.Length -eq 0) -and ($_.Thumbprint.ToString() -ne $CertThumbprint)) { # Same Services but not just installed
+                Write-Host "Removing certificate ($ThisSubject Expiring: $ThisNotAfter) from the Exchange Configuration. Thumbprint: $ThisThumbprint"
+                Remove-ExchangeCertificate -Thumbprint $_.Thumbprint -Confirm:$false
+                # remove old cert if specified
+                if ($RemoveOldCert) {
+                    Remove-OldCert $_.Thumbprint.ToString()
+                }
+            }
+        }
+
+        if (($NewCertInstalled) -and ('IIS' -in $ExchangeServices)) {
+            $TempFile = New-TemporaryFile
+            $IISCommand = (Get-Command 'iisreset.exe').Path
+            Start-Process -FilePath $IISCommand -ArgumentList '/RESTART' -NoNewWindow -RedirectStandardOutput $TempFile -Wait 
+            Get-Content $TempFile | Write-Host
+            Remove-Item $TempFile
         }
 
     }
