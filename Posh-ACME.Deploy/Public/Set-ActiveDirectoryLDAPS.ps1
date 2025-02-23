0fb5fc693e95eda34cf82d2b4e81aca8500925d5
Ryan Bolger <ryan-oss@xyto.cc>
2025-02-22 23:35:54 -0800

    Old NTDS cert is now always removed and RemoveOldCert only applies to LocalMachine cert

--- Posh-ACME.Deploy/Public/Set-ActiveDirectoryLDAPS.ps1
+++ Posh-ACME.Deploy/Public/Set-ActiveDirectoryLDAPS.ps1
@@ -23,24 +23,31 @@ function Set-ActiveDirectoryLDAPS {
         # Make sure the NTDS store exists
         $NtdsCertStore = 'HKLM:/Software/Microsoft/Cryptography/Services/NTDS/SystemCertificates/My/Certificates'
         if (-Not (Test-Path $NtdsCertStore)) {
-	        $null = New-Item $NtdsCertStore -Force
+            Write-Verbose "Existing NTDS cert store not found. Creating the necessary reg key."
+            $null = New-Item $NtdsCertStore -Force
         }
 
         # Look for the old cert thumbprint
         $oldThumbprint = Get-ChildItem $NtdsCertStore | Select-Object -First 1 | Select-Object -Expand PSChildName
 
         # Copy cert from local store to NTDS Store
+        Write-Verbose "Copying cert with thumbprint $CertThumbprint to NTDS cert store."
         Copy-Item -Path "$LocalCertStore/$CertThumbprint" -Destination $NtdsCertStore
 
-        # Remove old copies if necessary
-        if ($RemoveOldCert -and $oldThumbprint) {
-            Get-ChildItem $NtdsCertStore | Where-Object { $_.PSChildName -eq $oldThumbprint } | Remove-Item
-            Get-ChildItem $LocalCertStore | Where-Object { $_.PSChildName -eq $oldThumbprint } | Remove-Item
-        }
+        # Remove all certs except the new one from the NTDS store
+        Write-Verbose "Removing certs not matching thumbprint $CertThumbprint from NTDS cert store."
+        Get-ChildItem $NtdsCertStore | Where-Object { $_.PSChildName -ne $CertThumbprint } | Remove-Item
 
         # Command AD to update.
+        Write-Verbose "Triggering NTDS cert update."
         $dse = [adsi]'LDAP://localhost/rootDSE'
         [void]$dse.Properties['renewServerCertificate'].Add(1)
         $dse.CommitChanges()
+
+        # Remove the old cert from LocalMachine if asked to
+        if ($RemoveOldCert -and $oldThumbprint) {
+            Write-Verbose "Removing old cert with thumbprint $oldThumbprint from Local Machine cert store."
+            Get-ChildItem $LocalCertStore | Where-Object { $_.PSChildName -eq $oldThumbprint } | Remove-Item
+        }
     }
 }
